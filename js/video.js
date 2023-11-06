65d83b178846f127e0029e7817ecaf12974ff686
Justin Fischer <jfischer@adadapted.com>
2023-11-06 09:25:05 -0500

    first attempt at finished page

--- js/video.js
+++ js/video.js
@@ -7,7 +7,57 @@ window.addEventListener("load", function() {
 	video.loop = false;
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	video.volume = document.querySelector("#slider").value / 100;
+	document.querySelector('#volume').innerHTML = document.querySelector("#slider").value;
+	video.play();
+})
 
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+})
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.defaultPlaybackRate /= 1.1;
+	console.log(video.defaultPlaybackRate);
+})
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.defaultPlaybackRate *= 1.1;
+	console.log(video.defaultPlaybackRate)
+})
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 10 < video.duration){
+		video.currentTime += 10;
+	}
+	else {
+		video.currentTime = 0;
+	}
+	console.log(video.currentTime);
+})
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (document.querySelector("#mute").innerHTML == "Mute"){
+		document.querySelector("#mute").innerHTML = "Unmute";
+		video.muted = true;
+	}
+	else{
+		document.querySelector("#mute").innerHTML = "Mute";
+		video.muted = false;
+	}
+})
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = document.querySelector("#slider").value / 100;
+	document.querySelector('#volume').innerHTML = document.querySelector("#slider").value;
+	console.log(video.volume);
+})
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+})
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+})
