92e4c54a166bdceef32ab079510fcb6e014738f5
Justin Fischer <jfischer@adadapted.com>
2023-11-05 23:30:36 -0500

    init commit

---
+++ js/video.js
@@ -0,0 +1,12 @@
+var video;
+
+window.addEventListener("load", function() {
+	video.autoplay = false;
+	video.loop = false;
+	console.log("Hello")
+});
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
